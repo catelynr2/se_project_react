abfb672e15345767db189769f38b84667743c7c2
Catelyn Richard <catelyn.richard@yahoo.com>
2023-10-12 12:18:42 -0400

    utils error

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,4 +1,4 @@
-import { defaultClothingItems } from "../Util/constants";
+import { defaultClothingItems } from "../utils/constants";
 import WeatherCard from "../WeatherCard/WeatherCard";
 import ItemCard from "../ItemCard/ItemCard";
 import { useMemo } from "react";
